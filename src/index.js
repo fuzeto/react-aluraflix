9e5f365814d0645ba17f5e64506731c85ff3d9bc
Leonardo Fuzeto <fuzetolhf@gmail.com>
2020-08-01 08:49:59 -0300

    adding react-router to project

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import {
+  BrowserRouter,
+  Switch,
+  Route
+} from 'react-router-dom';
+import Home from './pages/Home';
+import CadastroVideo from './pages/Cadastro/Video';
+import CadastroCategoria from './pages/Cadastro/Categoria';
+
+function _404() {
+  return (
+    <div>
+      <h1>404 error page</h1>
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route component={_404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
